a01134d91b1ef82272cfc3d836336f72fe606f0e
Balinto <balintm2006@gmail.com>
2025-04-08 20:54:58 +0200

    Át kéne nézni az órait (new Person w Binding)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Neutrition.Resources;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,26 @@ namespace Neutrition
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        private Person person;
+
+        public Person Person
+        {
+            get { return person; }
+            set { person = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Person = new Person();
+            Person.Init();
+            DataContext = this;
+            
+
         }
     }
 }
---
+++ Resources/Person.cs
@@ -0,0 +1,54 @@
+﻿using Neutrition.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Neutrition.Resources
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public float StartWeight { get; set; }
+        public float CurWeight { get; set; }
+        public float GoalWeight { get; set; }
+        public float Height { get; set; }
+        public int Age { get; set; }
+        public float ActivityLevel { get; set; }
+        public DateTime GoalDate { get; set; }
+        //Mifflin-St Jeor Equation
+        public float TDEE { get {  return ((float)(10 * CurWeight + 6.25 * Height - 5 * Age + 5))*ActivityLevel; } }
+        public float DailyCaloricIntake { get { return TDEE - (((CurWeight-GoalWeight)*7700)/ (GoalDate - DateTime.Now.Date).Days); } }
+
+        public void Init()
+        {
+            //Read person.json from Data/person.json
+            //Deserialize it to Person object
+            //Set the properties of the Person object
+
+            string json = File.ReadAllText("Data/person.json");
+            JsonDocument jsonArray = JsonSerializer.Deserialize<JsonDocument>(json);
+            Name = jsonArray.RootElement.GetProperty("Name").GetString();
+            if(Name == "")
+            {
+                //show window
+                NewPerson newPerson = new NewPerson();
+                newPerson.ShowDialog();
+                if(newPerson.DialogResult == false)
+                {
+                    Application.Current.Shutdown();
+                }
+
+            }
+
+        }
+
+
+    }
+}
